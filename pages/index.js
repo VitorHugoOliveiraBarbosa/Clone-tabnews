d6440f8aed08fccc71e0496b4440189b817364c3
Vitor Hugo <107222031+VitorHugoOliveiraBarbosa@users.noreply.github.com>
2024-07-18 17:00:16 +0000

    troca o texto

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,4 @@
 function Home(){
-    return <h1>Agata, eu amo voce, se você me ama dá uma risadinha 🤭 </h1>
+    return <h1>Ágata, eu amo voce, se você me ama dá uma risadinha 🤭 </h1>
 }
 export default Home;
