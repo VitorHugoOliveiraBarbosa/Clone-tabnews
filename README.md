80f4af7cbc2c007ec3112bb91331ce627c675592
Vitor Hugo <107222031+VitorHugoOliveiraBarbosa@users.noreply.github.com>
2024-07-18 21:42:57 +0000

    adiciona scripts 'lint:check' e 'lint:fix'

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 # Clone-tabnews
+
 Treino de Programação feito acompanhando o Curso.dev
 para criar um app clone do tabnews.com.br
--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,4 @@
-function Home(){
-
-    return <h1>Ágata, eu amo voce, se você me ama dá uma risadinha 🤭 </h1>
-
-};
+function Home() {
+  return <h1>Ágata, eu amo voce, se você me ama dá uma risadinha 🤭 </h1>;
+}
 export default Home;
